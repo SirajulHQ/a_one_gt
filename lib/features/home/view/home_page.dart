97945119b38c065d1fccffe12da0918144cdc1af
Sirajul Haq <sirajulhaq3154@gmail.com>
2026-03-13 10:59:23 +0530

    fixed grid height

--- lib/features/home/view/home_page.dart
+++ lib/features/home/view/home_page.dart
@@ -296,9 +296,9 @@ class _HomePageState extends State<HomePage> {
               }, childCount: displayedProducts.length),
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
-                mainAxisSpacing: 14,
+                mainAxisSpacing: 0,
                 crossAxisSpacing: 14,
-                childAspectRatio: 0.8,
+                childAspectRatio: 0.95,
               ),
             ),
           ),
